0dc953fe9a64839650cfc515341e5eb7923ddfeb
Jeremy Goh <jeremy@moonfish.com>
2020-06-22 12:40:00 -0700

    edit payment button

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-alert("hi");
+var teamBuyButton = document.createElement("button");
+teamBuyButton.innerHTML = "Team Buy";
+teamBuyButton.setAttribute("class", "btn product-form__cart-submit btn--secondary-accent");
+teamBuyButton.addEventListener("click", function() {alert("You started a team buy")});
+
+var paymentButton = document.getElementsByClassName("shopify-payment-button")[0];
+paymentButton.insertAdjacentElement("beforebegin", teamBuyButton);
