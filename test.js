81d0512ac551976d721aa159635674b554d8eb8c
Jeremy Goh <jeremy@moonfish.com>
2020-06-24 13:49:29 -0700

    search by form

--- test.js
+++ test.js
@@ -1,7 +1,33 @@
-var teamBuyButton = document.createElement("button");
-teamBuyButton.innerHTML = "Team Buy";
-teamBuyButton.setAttribute("class", "btn product-form__cart-submit btn--secondary-accent");
-teamBuyButton.addEventListener("click", function() {alert("You started a team buy")});
+function getButtonToCopy() {
+  var addToCartForm = document.querySelectorAll('form[action="/cart/add"]');
+  if (addToCartForm.length === 0) {
+    console.log("no add to cart form");
+    return null;
+  }
 
-var paymentButton = document.getElementsByClassName("shopify-payment-button")[0];
+  addToCartForm = addToCartForm[0];
+
+  var buttons = addToCartForm.getElementsByTagName("button");
+  if (buttons.length === 0) {
+    console.log("no buttons to copy");
+    return null;
+  }
+  return buttons[0];
+}
+
+var button = getButtonToCopy();
+
+var teamBuyButton = document.createElement("button");
+teamBuyButton.innerHTML = "Buy With Friends";
+
+teamBuyButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("You started a team buy");
+});
+
+var paymentButton = document.getElementsByClassName(
+  "shopify-payment-button"
+)[0];
+teamBuyButton.style.cssText = window.getComputedStyle(button, null).cssText;
+
 paymentButton.insertAdjacentElement("beforebegin", teamBuyButton);
